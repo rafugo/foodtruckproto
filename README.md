f74143beb2736fcdfe3d3fda2782353bffc39ba3
Rafael Fueyo-Gomez <rfueyogo@caltech.edu>
2018-09-09 14:12:45 -0700

    got a decent address parser working

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # foodtruckproto
 
+# Getting started
+Have to install selenium (python package) for the current twitterscraper. I did
+it via `pip install selenium`. You need python 3 tho.
+
+The address parser currently is `pip install usaddress` which I got from here: 
+https://github.com/datamade/usaddress.
+
+Lastly, you will need to download the chrome browser as well as the chrome 
+driver, and then put the chrome driver in your PATH that python looks in.
+
 ## TODO:
 ### Current Goals
-[ ] Figure out how to parse addresses appropriately
-
-[ ] Figure out how to parse dates/times appropriately
+- [ ] Figure out how to parse addresses appropriately
+- [ ] Figure out how to parse dates/times appropriately
 
 ### Future Goals
-[ ] Create a twitter scraper class with useful functions
-
-[ ] Make a list of food trucks to test on
+- [ ] Create a twitter scraper class with useful functions
+- [ ] Make a list of food trucks to test on
 
 
 ## Food trucks considered only in LA
 Kogi, NoMad, ...
-
-## Useful Links:
-Library for parsing addresses: https://pypi.org/project/pyap/
--- twitterscraper.py
+++ twitterscraper.py
@@ -1,23 +1,79 @@
 import time
+import pyap
+import usaddress
+import re
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# simple function for parsing addresses using pyap
+def parseAddress(text):
+    addresses = pyap.parse(text, country='US')
+    return addresses
+
+# a much better parsing addresses function using usaddress
+def parseAddressV2(text):
+    lastSeenLabel = ''
+
+    parsedList = usaddress.parse(text)
+
+    # time to create a list of the addresses present.
+    addressList = []
+    tempAddress = ''
+
+    labelTypes = ['AddressNumber', 'StreetNamePreDirectional',
+                    'StreetName', 'StreetNamePostType',
+                    'ZipCode', 'PlaceName']
+
+    for pair in parsedList:
+        if pair[1] in labelTypes:
+            tempAddress += (re.sub('[!@#(),\n$]', '', pair[0]) + " ")
+            
+        elif (lastSeenLabel == 'StreetNamePostType' 
+                and pair[1] == 'OccupancyIdentifier'):
+            tempAddress += (re.sub('[!@#(),\n$]', '', pair[0]) + " ")
+
+        elif tempAddress != '':
+            addressList.append(tempAddress.strip())
+            tempAddress = ''
+
+        lastSeenLabel = pair[1]
+
+    return addressList
+
+
+
+
 browser = webdriver.Chrome()
 base_url = u'https://twitter.com/'
-handle = u'kogibbq'
+handle = u'NoMadTruckLA'
 url = base_url + handle
 
 browser.get(url)
-time.sleep(1)
+time.sleep(2)
 
 body = browser.find_element_by_tag_name('body')
 
-for _ in range(5):
-    body.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
-
 tweets = browser.find_elements_by_class_name('tweet-text')
 
+
+count = 1
 for tweet in tweets:
+    
+    if count > 10:
+        count+=1
+        continue
+    
+    tweetAddresses = parseAddressV2(tweet.text)
+    print("Tweet " + str(count) + " Text:")
     print(tweet.text)
+    print()
+
+    print("Address parsing list: ")
+    print(tweetAddresses)
+    print('\n\n\n\n')
+
+    count+=1
+
+
+browser.close()
