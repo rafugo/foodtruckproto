1d6df6ebd8c8d167916629db13b9599234ad145f
alettenb <alettenb@caltech.edu>
2018-09-10 14:49:18 -0700

    More edits.

--- food-truck-web/src/App.js
+++ food-truck-web/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Promise from 'bluebird';
 
 class App extends Component {
+
+  foodTruckArray = [
+    {name: 'Kobe Beef Truck', genres: ['Korean', 'Mexican'], id: null},
+    {name: 'Grilled Cheesewich', genres: ['American'], id: null},
+  ];
+
+  mapArray = (arr) => (
+    arr.map(str =>
+      <div className='App-box' >
+        <p>{str.name}</p>
+        <p>{str.genres.reduce((acc, cur) => acc === '' ? cur : `${acc}, ${cur}`, '')}</p>
+      </div>
+    )
+  );
+
   render() {
+    const testDiv = this.mapArray(this.foodTruckArray);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the project</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+          {testDiv}
+        </div>
       </div>
     );
   }
--- food-truck-web/src/index.js
+++ food-truck-web/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App arr={['1', '2', '3']} />, document.getElementById('root'));
 registerServiceWorker();
